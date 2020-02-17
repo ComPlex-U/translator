7845a7a153fc2c18b2cee20fab455eda65c90b75
johngstephen <jonhgstephen@gmail.com>
2020-02-17 23:21:05 +0000

    Create Translator.py

---
+++ Translator.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from googletrans import Translator
+
+win = tk.Tk()
+win.title("Translator")
+win.geometry("200x70")
+
+def translation():
+    word = entry.get()
+    translator = Translator(service_urls=['translate.google.com'])
+    translation1 =translator.translate(word,dest="ru")
+    label1 = tk.Label(win, text=f"translate in russian :{translation1.text}",bg="yellow")
+    label1.grid(row=2, column=0)
+label = tk.Label(win, text="Enter word :")
+label.grid(row=0, column=0, sticky="w")
+entry = tk.Entry(win)
+entry.grid(row=1, column=0)
+button = tk.Button(win, text="Translate", command=translation)
+button.grid(row=1, column=2)
+win.mainloop()
